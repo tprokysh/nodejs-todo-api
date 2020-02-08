a2534cc632ddedf7a7a91574808138f0d97d44f2
rotttshild <prokish.tal@icloud.com>
2020-02-08 19:48:40 +0200

    add router and config

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-global.console.log("hello");
+import config from "../config/config.json";
+import db from "../config/db.json";
+import router from "./router";
+
+const app = require("express")();
+
+const environment = process.env.NODE_ENV || "development";
+
+global.gConfig = config;
+global.gDB = db[environment];
+
+app.use(global.gConfig.apiPrefix, router);
+
+app.listen(global.gConfig.nodePort, () => {
+    global.console.log(
+        `${global.gConfig.appName} listening on port ${global.gConfig.nodePort}`
+    );
+});
