d33a8f14eef2099af96317c42dc7fb1e1f1fa201
rotttshild <prokish.tal@icloud.com>
2020-02-12 12:35:50 +0200

    refactor server

--- runner.js
+++ runner.js
@@ -1,9 +1,5 @@
-import App from "./lib/App";
 import Server from "./lib/Server";
 
 require("@babel/register");
 
-const app = new App();
-const server = new Server();
-
-server.createServer(app.app);
+export default new Server();
